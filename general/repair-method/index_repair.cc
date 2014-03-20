9b14216a2e7fbfb3da7723dea8504db0ee5fd672
Yan <yankrasny@gmail.com>
2014-03-19 20:39:40 -0400

    restructuring - forgot to do git add

---
+++ general/repair-method/index_repair.cc
@@ -0,0 +1,172 @@
+/*
+ * index_repair.cc
+ *
+ * Author: Yan Krasny
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <fstream>
+
+//#define MAXSIZE 250939073
+#define	MAXSIZE 81985059
+struct binfo
+{
+    int numFragApplications, numDistinctFrags, numPostings;
+};
+
+#include "general_partition_repair.h"
+#include "../../util/indexer.h"
+
+
+using namespace std;
+
+//#define PRINTTREE 1
+int* buf;
+int* wcounts;
+int* wsizes;
+partitions<CutDocument2>* partitionAlgorithm;
+
+int ndoc = 0;
+int* mhbuf;
+int* hbuf;
+unsigned char* tree_buf;
+indexer* ib;
+
+int dothejob(vector<vector<unsigned> >& versions, int docId, float wsize)
+{
+    char fn[256];
+    memset(fn, 0, 256);
+    sprintf(fn, "/data4/jhe/proximity/model/test/%d", docId);
+    FILE* f = fopen(fn, "r");
+    int total = 1;
+    int a,b,c,d,e,fs;
+    while ( fscanf(f, "%d\t%d\t%d\t%d\t%d\t%d\n", &a, &b, &c, &d, &e, &fs)>0){
+        wsizes[total] = a;
+        total++;
+    }
+    wsizes[0] = 5;
+    fclose(f);
+
+    memset(fn, 0, 256);
+    sprintf(fn, "/data4/jhe/proximity/general/meta/%d", docId);
+    f = fopen(fn, "rb");
+    int size;
+    fread(&size, sizeof(unsigned), 1, f);
+    fread(mhbuf, sizeof(unsigned), size, f);
+    fclose(f);
+    
+    memset(fn, 0, 256);
+    sprintf(fn, "/data4/jhe/proximity/minWinnowing/meta/%d", docId);
+    f = fopen(fn, "rb");
+    fread(&size, sizeof(unsigned), 1, f);
+    fread(hbuf, sizeof(unsigned), size, f);
+    fclose(f);
+
+    iv* invertedLists = new iv[numVersions];
+    partitionAlgorithm->init();
+
+    // I think wsizes is window sizes, and total is the size of that array
+    // TODO these vars should not be hardcoded, if time permits we'll change it
+    float fragmentationCoefficient = 1.0;
+    float minFragSize = 100;
+    unsigned numLevelsDown = 5;
+    int numFrags = partitionAlgorithm->fragment(versions, invertedLists, 
+        fragmentationCoefficient, minFragSize, numLevelsDown);
+    
+    printf("Finished partitioning!\n");
+
+    partitionAlgorithm->addFragmentsToHeap(wsize);
+    partitionAlgorithm->selectGoodFragments(invertedLists, wsize);
+    partitionAlgorithm->populatePostings(versions, docId);
+
+    for (int i = 0; i < partitionAlgorithm->add_list_len; i++)
+    {
+        // partitionAlgorithm->add_list is a posting*
+        // wid is wordID
+        // pos is position (most likely)
+        // vid is versionID, and we're using version IDs as doc IDs
+        ib->insert_term(partitionAlgorithm->add_list[i].wid, 
+            partitionAlgorithm->add_list[i].pos, partitionAlgorithm->add_list[i].vid);
+    }
+    
+    delete [] invertedLists;
+    ndoc += numVersions;
+    return partitionAlgorithm->add_list_len;
+}
+
+
+int main(int argc, char**argv)
+{
+    // Make the same changes wrt params as you did in diff2_repair.cc
+    if (argc < 2)
+    {
+        printf("Same params as diff2_repair\n");
+        exit(0);
+    }
+    partitionAlgorithm = new partitions<CutDocument2>(atoi(argv[1]),atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
+    wsizes = new int[1000];
+    buf = new int[MAXSIZE];
+    mhbuf = new int[MAXSIZE];
+    hbuf = new int[MAXSIZE];
+    tree_buf = new unsigned char[MAXSIZE];
+
+    FILE* fcomplete = fopen("/data/jhe/wiki_access/completeFile", "rb");
+    FILE* fnumv = fopen("/data/jhe/wiki_access/numv", "rb");
+    int doccount;
+    fread(&doccount, sizeof(unsigned), 1, fnumv);
+    int* numv = new int[doccount];
+    fread(numv, sizeof(unsigned), doccount, fnumv);
+    ib = new indexer(400000000, atoi(argv[5]), 1);
+
+    FILE* fword_size = fopen("/data/jhe/wiki_access/word_size", "rb");
+    unsigned total_ver;
+    fread(&total_ver, sizeof(unsigned), 1, fword_size);
+    wcounts = new int[total_ver];
+    fread(wcounts, sizeof(unsigned), total_ver, fword_size);
+    fclose(fword_size);
+
+    int ptr = 0;
+    FILE* f2 = fopen("result", "rb");
+    fread(&doccount, sizeof(unsigned), 1, f2);
+    float* result_buf = new float[doccount];
+    fread(result_buf, sizeof(unsigned), doccount, f2);
+    fclose(f2);
+
+    ofstream fout("index_list");
+    for (int i = 0; i < doccount; i++)
+    {
+        int total = 0;
+        for (int j = 0; j < numv[i]; j++)
+        {
+            total += wcounts[ptr+j];
+        }
+
+        fread(buf, sizeof(unsigned), total, fcomplete);
+
+        // if (i < atoi(argv[2])) {
+        //     ptr += numv[i];
+        //     continue;
+        // }
+
+        int totalCount = dothejob(buf, &wcounts[ptr], i, numv[i], result_buf[i]);
+        fout << result_buf[i] << '\t' << totalCount << endl;
+        printf("Complete:%d\n", i);
+        ptr += numv[i];
+        
+        // if (i >= atoi(argv[3]))
+        //     break;
+    }
+    
+    fout.close();
+
+    ib->dump();
+    
+    FILE* fscud = fopen("SUCCESS!", "w");
+    fclose(fscud);
+    
+    delete partitionAlgorithm;
+    return 0;
+}
---
+++ general/repair-method/iv_repair.h
@@ -0,0 +1,184 @@
+#ifndef IV_H_
+#define IV_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <list>
+#include <map>
+#include "../vb.h"
+#include "../readwritebits.h"
+
+#define MAX_NUM_NODES 1000000
+
+using namespace std;
+struct frag_ptr
+{
+    int ptr;
+    int off;
+};
+
+bool operator == (const frag_ptr& f1, const frag_ptr& f2)
+{
+    if (f1.ptr == f2.ptr && f1.off == f2.off)
+        return true;
+    else
+        return false;
+}
+
+struct node
+{
+    int nid;
+    frag_ptr fptr;
+    int start;
+    int end;
+};
+
+int compareiv(const void* a1, const void* a2)
+{
+    node* n1 = (node*)a1;
+    node* n2 = (node*)a2;
+
+    int ret = n1->start - n2->start;
+    int ret2 = n1->end - n2->end;
+    if (ret == 0)
+        return ret2;
+    else
+        return ret;
+}
+
+class iv
+{
+public:
+    iv()
+    {
+        nID = 0;
+        nbuf = new node[MAX_NUM_NODES];
+        sort_nbuf = new node[MAX_NUM_NODES];
+        inlist = NULL;
+    }
+
+    ~iv()
+    {
+        delete[] nbuf;
+        delete[] sort_nbuf;
+        delete[] inlist;
+    }
+
+    int nID;
+    list<int>* inlist;
+    node* nbuf;
+    node* sort_nbuf;
+
+    int storeiv(unsigned char* buf)
+    {
+        int id = nID;
+        unsigned char* buf_ptr = buf;
+        VBYTE_ENCODE(buf_ptr, id);
+
+        for ( int i = 0; i < id; i++)
+        {
+            int size = inlist[i].size();
+            VBYTE_ENCODE(buf_ptr, size);
+            for ( list<int>::iterator its = inlist[i].begin(); its != inlist[i].end(); its++)
+            {
+                int k = (*its);
+                VBYTE_ENCODE(buf_ptr, k);
+            }
+        }
+
+        return buf_ptr - buf;
+    }
+
+    int loadiv(unsigned char* buf)
+    {
+        unsigned char* buf_ptr = buf;
+        int id;
+        VBYTE_DECODE(buf_ptr, id);
+        nID = id;
+        inlist = new list<int>[id];
+        nbuf = new node[id];
+        for (int i = 0; i < id; ++i)
+        {
+            int size;
+            VBYTE_DECODE(buf_ptr, size);
+            for (int j = 0; j < size; ++j)
+            {
+                int k;
+                VBYTE_DECODE(buf_ptr, k);
+                inlist[i].push_back(k);
+            }
+        }
+    }
+    
+    /* 
+     * insert a fragment information to the inverted list structure
+     */
+    int insert(const frag_ptr& fptr, int start, int end)
+    {
+        --end;
+        nbuf[nID].fptr = fptr;
+        nbuf[nID].start = start;
+        nbuf[nID].end = end;
+        nbuf[nID].nid = nID;
+        sort_nbuf[nID] = nbuf[nID];
+        ++nID;
+        if (nID > MAX_NUM_NODES)
+        {
+            printf("Too many nodes in class iv...\n");
+            exit(0);
+        }
+        return nbuf[nID - 1].nid;
+    }
+
+    bool intersect(const node& n1, const node& n2)
+    {
+        if (n1.start > n2.end || n1.end < n2.start)
+            return false;
+        else
+            return true;
+    }
+
+    int complete()
+    {
+        inlist = new list<int>[nID];
+        for (int i = 0; i < nID; ++i)
+        {
+            for (int j = i + 1; j < nID; ++j)
+            {
+                if (intersect(sort_nbuf[i], sort_nbuf[j]))
+                {
+                    int idx = sort_nbuf[i].nid;
+                    int idx2 = sort_nbuf[j].nid;
+                    inlist[idx].push_back(idx2);
+                    inlist[idx2].push_back(idx);
+                }
+                //else
+                //	break;
+            }
+        }
+    }
+
+    int find_conflict(int nID, vector<FragmentApplication>& li, int idx, frag_ptr* fbuf)
+    {
+        int size = inlist[nID].size();
+        list<int>::iterator its;
+        int ptr = 0;
+        for (its = inlist[nID].begin(); its != inlist[nID].end(); its++)
+        {
+            frag_ptr& fm = nbuf[*its].fptr;
+            fbuf[ptr].ptr = fm.ptr;
+            fbuf[ptr].off = fm.off;
+            if (idx == fm.ptr)
+            {
+                li.at(fm.off).isVoid = true;
+            }
+            else
+            {
+                ptr++;
+            }
+        }
+        return ptr;
+    }
+};
+
+#endif
